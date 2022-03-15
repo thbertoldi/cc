8e7105ede7b70f84479100ea445cb9ee71412e64
Thiago H. Bertoldi <thb96@hotmail.com>
2022-03-15 00:07:33 -0300

    Plots
    Signed-off-by: Thiago H. Bertoldi <thb96@hotmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 
 def load_data():
     actions = np.load("actions.npy", allow_pickle=True)
     states = np.load("states.npy", allow_pickle=True)
-    print(len(actions))
-    print(len(states))
+    print(f"Tenho {len(actions)} ações")
+    print(f"Tenho {len(states)} estados")
+
+    fig, ax = plt.subplots(3, 1)
+    for i in range(3):
+        ax[i].plot(actions[:, i])
+    plt.show()
+
+    plt.imshow(states[1000, :, :, :])
+    plt.show()
 
 if __name__ == "__main__":
     load_data()
